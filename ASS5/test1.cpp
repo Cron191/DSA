8714c82982b92cbfa82b230a9143092cda0f39a8
qcron <23520205@gm.uit.edu.vn>
2024-12-03 13:30:17 +0700

    DSA

--- ASS5/test1.cpp
+++ ASS5/test1.cpp
@@ -1,58 +1,20 @@
 #include <iostream>
 using namespace std;
-
-struct TNODE {
-	int key;
-	TNODE* pLeft;
-	TNODE* pRight;
-};
-typedef TNODE* TREE;
-
-TNODE *CreateTNode(int x)
+int main()
 {
-    TNODE *p = new TNODE; 
-    p->key = x;           
-    p->pLeft = NULL;
-    p->pRight = NULL;
-    return p;
-}
-int Insert(TREE &T, TNODE *p)
-{
-    if (T)
+    long long a;
+    cin >> a;
+    long long cnt = 0;
+
+    while (a != 1)
     {
-        if (T->key == p->key)
-            return 0;
-        if (T->key > p->key)
-            return Insert(T->pLeft, p);
-        return Insert(T->pRight, p);
+        if (a % 2 == 0)
+            a /= 2;
+        else
+            a = a * 3 + 1;
+        cnt++;
+        cout << a << " ";
     }
-    T = p;
-    return 1;
+    cout << endl
+         << cnt;
 }
-void CreateTree(TREE &T)
-{
-    while (cin)
-    {
-        int x;
-        cin >> x;
-        if (x == -1)
-            break;
-        TREE p = CreateTNode(x);
-        Insert(T, p);
-    }
-}
-TREE FindMax(TREE T)
-{
-    while(T->pRight!=NULL) T=T->pRight;
-    return T;
-}
-int main() {
-	TREE T; //hay: TNODE* T;
-	T = NULL; // Khoi tao cay T rong, or: CreateEmptyTree(T)
-	CreateTree(T);
-
-	if(T==NULL) cout << "Empty Tree.";
-	else cout << FindMax(T)->key;
-	return 0;
-}
-
--- sort.cpp
+++ sort.cpp
@@ -42,6 +42,20 @@ void Insertion_Sort(int a[], int n)
     }
 }
 
+void Insertion_Sort1(int a[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int x = a[i];
+        int pos = i;
+        while (pos > 0 && a[pos - 1] > x)
+        {
+            a[pos] = a[pos - 1];
+            pos--;
+        }
+        a[pos] = x;
+    }
+}
 // QUICK_SORT O(N^N) O(Nlog(N)) O(Nlog(N))
 void Quick_Sort(int a[], int l, int r)
 {
@@ -65,6 +79,27 @@ void Quick_Sort(int a[], int l, int r)
         Quick_Sort(a, i, r);
 }
 
+void Quick_Sort1(int a[], int l, int r)
+{
+    int i = l, j = r, pivot = a[(i + j) / 2];
+    while (i <= j)
+    {
+        while (a[i] < pivot)
+            i++;
+        while (a[j] > pivot)
+            j--;
+        if (i <= j)
+        {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    }
+    if (l < j)
+        Quick_Sort1(a, l, j);
+    if (i < r)
+        Quick_Sort1(a, i, r);
+}
 // MERGE_SORT O(Nlog(N)) O(Nlog(N)) O(Nlog(N))
 void Merge(int a[], int l, int m, int r)
 {
@@ -98,6 +133,38 @@ void Merge_Sort(int a[], int l, int r)
     }
 }
 
+void Merge1(int a[], int l, int m, int r)
+{
+    int len1 = m - l + 1, len2 = r - m;
+    int left[len1], right[len2];
+    for (int i = 0; i < len1; i++)
+        left[i] = a[i + l];
+    for (int i = 0; i < len2; i++)
+        right[i] = a[i + m + 1];
+    int i = 0, j = 0, k = l;
+    while (i < len1 && j < len2)
+    {
+        if (left[i] <= right[j])
+            a[k++] = left[i++];
+        else
+            a[k++] = right[j++];
+    }
+    while (i < len1)
+        a[k++] = left[i++];
+    while (j < len2)
+        a[k++] = right[j++];
+}
+void Merge_Sort1(int a[], int l, int r)
+{
+    int m = (l + r) / 2;
+    if (l < r)
+    {
+        Merge_Sort1(a, l, m);
+        Merge_Sort1(a, m + 1, r);
+        Merge1(a, l, m, r);
+    }
+}
+
 // HEAP_SORT O(Nlog(N)) O(Nlog(N)) O(Nlog(N))
 void Heapify(int a[], int n, int i)
 {
@@ -114,9 +181,9 @@ void Heapify(int a[], int n, int i)
 }
 void Heap_Sort(int a[], int n)
 {
-    for (int i = (n - 1) / 2; i > 0; i--)
+    for (int i = (n - 1) / 2; i >= 0; i--)
         Heapify(a, n, i);
-    for (int i = n - 1; i > 0; i--)
+    for (int i = n - 1; i >= 0; i--)
     {
         swap(a[i], a[0]);
         n--;
@@ -124,7 +191,32 @@ void Heap_Sort(int a[], int n)
     }
 }
 
+void Heapify1(int a[], int n, int i)
+{
+    int left = 2 * i + 1, right = 2 * i + 2;
+    int max = i;
+    if (left < n && a[left] > a[max])
+        max = left;
+    if ((right < n && a[right] > a[max]))
+        max = right;
+    if (max != i)
+    {
+        swap(a[i], a[max]);
+        Heapify1(a, n, max);
+    }
+}
 
+void Heap_Sort1(int a[], int n)
+{
+    for (int i = (n - 1) / 2; i >= 0; i--)
+        Heapify1(a, n, i);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(a[i], a[0]);
+        n--;
+        Heapify1(a, n, 0);
+    }
+}
 int main()
 {
     int n;
@@ -134,10 +226,10 @@ int main()
         cin >> a[i];
     // Selection_sort(a, n);
     // Interchange_Sort(a, n);
-    // Insertion_Sort(a, n);
-    // Quick_Sort(a, 0, n - 1);
-    // Merge_Sort(a, 0, n - 1);
-    // Heap_Sort(a, n);
+    // Insertion_Sort1(a, n);
+    // Quick_Sort1(a, 0, n - 1);
+    // Merge_Sort1(a, 0, n - 1);
+    Heap_Sort1(a, n);
     for (int x : a)
         cout << x << " ";
 }
